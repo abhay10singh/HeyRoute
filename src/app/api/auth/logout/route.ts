807c01ba8b9c6f659f7ed7311ae051734270d8e9
abhay10singh <154545879+abhay10singh@users.noreply.github.com>
2025-05-20 01:15:16 +0530

    Update route.ts

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -17,7 +17,7 @@ export async function POST() {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
-      domain: process.env.NODE_ENV === 'production' ? `.yourdomain.com` : 'localhost'
+
     });
 
     return response;
