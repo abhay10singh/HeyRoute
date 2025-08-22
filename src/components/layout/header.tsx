1ba8645fd933578a5b761c4278c376ff3a1b46c7
Abhay Singh <154545879+abhay10singh@users.noreply.github.com>
2025-08-23 02:12:09 +0530

    Fixed Toggle Button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -117,10 +117,10 @@ if (!isMounted) return null;
                   <UserPlus className="mr-1 h-4 w-4" /> Sign Up
                 </Link>
               </Button>
-              <ThemeToggleButton   variant = 'gif' url = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnpwM2h0aDAzeHhnbHcxMDhkM3hoZTc0OTE3dmU1bXNoNHY2YXp3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/I6GC6WHpBCJJvI0IVh/giphy.gif"/>
             </>
           )}
         </div>
+         <ThemeToggleButton   variant = 'gif' url = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnpwM2h0aDAzeHhnbHcxMDhkM3hoZTc0OTE3dmU1bXNoNHY2YXp3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/I6GC6WHpBCJJvI0IVh/giphy.gif"/>
       </div>
     </header>
   )
